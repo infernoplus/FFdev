dc5af0db4145f214c62b873b6669a1d136d4a131
Patrick <dmtinfernocide@gmail.com>
2015-11-06 17:19:12 -0600

    Update tile.js

--- src/js/display/tile.js
+++ src/js/display/tile.js
@@ -5,8 +5,8 @@ var tile = {};
 tile.init = function() {
 
 	tile.tileSet = image.get('img/tile/basic.png');
-	tile.res = 32;  //Resolution of a single tile
-	tile.sheetRes = 128;//Resolution of the full sheet
+	tile.res = 72;  //Resolution of a single tile
+	tile.sheetRes = 936;//Resolution of the full sheet
 };
 
 //Takes the index of a tile and gives the top left corner of it. Tile 0 is 0,0 and tile 1 is 16,0... etc...
