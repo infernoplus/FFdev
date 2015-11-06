14c09c17fb2d64556c6ecb7f626d1cd7a1b4d085
Patrick <dmtinfernocide@gmail.com>
2015-11-06 17:19:50 -0600

    Create player.js

---
+++ src/js/game/player.js
@@ -0,0 +1,34 @@
+"use strict";
+
+var player = {};
+
+player.init = function() {
+	player.npc = undefined; //The object that the player is controlling
+};
+
+player.step = function() {
+ if(ui.isActive() || game.inBattle())
+	return;
+ var x=0,y=0;
+ if(input.keys[87])
+	y--;
+ if(input.keys[83])
+	y++;
+ if(input.keys[65])
+	x--;
+ if(input.keys[68])
+	x++;
+ player.npc.move(x,y);
+
+ if(input.keys[69]) {
+	var t;
+	switch(player.npc.look) {
+	 case 0 : t = object.getObjectAtPos(player.npc.pos.x-1,player.npc.pos.y); break;
+	 case 1 : t = object.getObjectAtPos(player.npc.pos.x+1,player.npc.pos.y); break;
+	 case 2 : t = object.getObjectAtPos(player.npc.pos.x,player.npc.pos.y-1); break;
+	 default : t = object.getObjectAtPos(player.npc.pos.x,player.npc.pos.y+1); break;
+	}
+	if(t !== undefined)
+	 ui.openChat(t.name, t.message);
+ }
+};
